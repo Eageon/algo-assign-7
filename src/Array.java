1db934f6f6806e759939d2f2d66fa1ae696f9d23
jxy132330 <jxy132330@ECOAL71840.campus.ad.utdallas.edu>
2013-11-06 17:28:43 -0600

    Algorithm complete

--- src/Array.java
+++ src/Array.java
@@ -21,26 +21,33 @@ public class Array {
 			// To make the i inclusive, use i+1 as parameter, i+1 is exclusive
 			int swap = array[i];
 			array[i] = array[j];
-			array[j] = swap;		
+			array[j] = swap;
 		}
 	}
-	
+
 	public void sort(Sorter sorter) {
 		sorter.sort(array);
 	}
-	
+
 	public boolean verify() {
 		for (int i = 1; i < array.length; i++) {
-			if(array[i-1]> array[i])
+			if (array[i - 1] > array[i])
 				return false;
 		}
-		
-		return true;	
+
+		return true;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int runTimes = 1000;
+		int[] n = { 10000, 50000, 100000, 200000, 400000, 800000 };
+		Array heapArray, insertArray, mergeArray, quickArray;
+		
+		for (int i : n) {
+			heapArray = new Array(i);
+			heapArray.initialize();
+		}
 	}
 
 }
--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,10 +1,39 @@
-
 public class HeapSort implements Sorter {
 
 	@Override
 	public void sort(int[] array) {
-		// TODO Auto-generated method stub
-
+		for (int i = array.length / 2 - 1; i >= 0; i--)
+			/* buildHeap */
+			percDown(array, i, array.length);
+		for (int i = array.length - 1; i > 0; i--) {
+			swapReferences(array, 0, i); /* deleteMax */
+			percDown(array, 0, i);
+		}
 	}
 
+	private static int leftChild(int i) {
+		return 2 * i + 1;
+	}
+
+	private void percDown(int[] array, int i, int n) {
+		int child;
+		int tmp;
+
+		for (tmp = array[i]; leftChild(i) < n; i = child) {
+			child = leftChild(i);
+			if (child != n - 1 && array[child] < array[child + 1])
+				child++;
+			if (tmp < array[child])
+				array[i] = array[child];
+			else
+				break;
+		}
+		array[i] = tmp;
+	}
+	
+	private final void swapReferences(int[] array, int a, int b) {
+		int tmp = array[a];
+		array[a] = array[b];
+		array[b] = tmp;
+	}
 }
